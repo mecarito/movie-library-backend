102f9e474978df44ebd502cb5d83e057f58cbc33
mecarito <pieritomecar@gmail.com>
2022-10-11 03:26:07 +0300

    fetching upcommmig movies

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { request } from 'undici';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  url = process.env.BASE_URL;
+  apiKey = process.env.API_KEY;
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async getUpcommingMovies() {
+    const { statusCode, headers, trailers, body } = await request(
+      `${this.url}movie/upcoming?api_key=${this.apiKey}`,
+    );
+    const data = await body.json();
+    return data;
   }
 }
