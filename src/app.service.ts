e1ac9100f0d81e6d81431e6503ebe7b4f544344f
mecarito <pieritomecar@gmail.com>
2022-10-11 05:10:36 +0300

    minor change

--- src/app.service.ts
+++ src/app.service.ts
@@ -73,7 +73,6 @@ export class AppService {
   }
 
   async sortMovies(genre: number) {
-    console.log(genre);
     const result = [];
     `https://api.themoviedb.org/3/discover/movie?api_key={{api_key}}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=28,53`;
 
